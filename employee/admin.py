8745db4ffc193925e12957e10547846b597f4e4b
Krasikoff <z1r0-d@yandex.ru>
2024-09-29 01:48:33 +0300

    Add search and filter in adminpage.

--- employee/admin.py
+++ employee/admin.py
@@ -10,10 +10,17 @@ from .models import Skill
 admin.site.register(Team)
 admin.site.register(Competence)
 admin.site.register(Skill)
-@admin.register(Rating)
-class PostAdmin(admin.ModelAdmin):
-#admin.site.register(Rating)
-    search_fields = ['fio',]
-admin.site.register(Employee)
 admin.site.register(Position)
+
+
+@admin.register(Rating)
+class RaitingAdmin(admin.ModelAdmin):
+    list_filter = ['fio',]
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    search_fields = ['fio',]
+
+
 admin.site.empty_value_display = 'Не задано'
