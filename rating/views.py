4c51fbcaec9ea8f6a283b61aba06b7c7d73bda36
Krasikoff <z1r0-d@yandex.ru>
2024-09-30 14:56:58 +0300

    Add output raining table.

--- rating/views.py
+++ rating/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import(
     get_list_or_404, get_object_or_404, render
 )
-from employee.models import Employee
+from employee.models import Employee, Rating
 
-
-rating_catalog = get_list_or_404(Employee)
 
 
 def rating_detail(request, pk):
-    line = get_object_or_404(Employee, pk=pk)
+    employee = get_object_or_404(Employee, pk=pk)
+    rating_lines = get_list_or_404(Rating.objects.filter(fio=pk))
     template = 'rating/detail.html'
-    context = {'rating': line}
+    context = {'rating_lines': rating_lines, 'employee': employee}
     return render(request, template, context)
 
 
 def rating_list(request):
+    rating_catalog = get_list_or_404(Employee)
     template = 'rating/list.html'
     context = {'rating_list': rating_catalog}
     return render(request, template, context)
