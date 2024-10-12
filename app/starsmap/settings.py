57d256a1b4130acf20d04bbfcfd2d06066fafb9b
Sobiyk <kirill.sobkovskii@yandex.ru>
2024-10-12 06:52:38 +0300

    fix

--- app/starsmap/settings.py
+++ app/starsmap/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if app_stage == 'prod':
-    DEBUG = False
+    DEBUG = True
 else:
     DEBUG = True
 
