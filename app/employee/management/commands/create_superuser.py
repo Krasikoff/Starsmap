9a3c40b75998e8655cc6ed9463f9bcb94b48d22a
Krasikoff <z1r0-d@yandex.ru>
2024-10-15 17:00:43 +0300

    Add post del candidate some more add in readme.

--- app/employee/management/commands/create_superuser.py
+++ app/employee/management/commands/create_superuser.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
-
 from employee.models import User
+
 from starsmap import settings
 
 User = get_user_model()
--- app/starsmap/urls.py
+++ app/starsmap/urls.py
@@ -14,16 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from django.conf.urls import url
+
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
+"""
+URL api, админки, страниц, свагера.
+
+path('rating/', include('rating.urls', namespace='rating')) - рабочая, нужно только добавить в urlpatterns.
+"""
+
 urlpatterns = [
     path('', include('homepage.urls', namespace='homepage')),
-    path('rating/', include('rating.urls', namespace='rating')),
     path('about/', include('about.urls', namespace='about')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
